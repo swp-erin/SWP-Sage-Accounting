782f63bc5c67af77176dbdc135f20e2055661e3b
swp-erin <erin@smartwebpros.com>
2020-09-10 15:50:35 -0400

    Create swp-sage-accounting.php
    add main plugin file

---
+++ swp-sage-accounting.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: SWP Sagw Accounting
+Plugin URI: http://www.smartwebpros.com/
+Version: 1.0
+Author: SmartWebPros.com Inc.
+Author URI: http://www.smartwebpros.com/
+Description: Allows clinets to log in and view thier invoices
+*/
+
+// define constance
+
+define('SSA_PLUGIN_FILE', __FILE__);
+
+// add necessary plugin files
+
+//require_once(plugin_dir_path( __FILE__ ) . 'includes/functions.php');
+
+
+function ssa_init() {
+	// register scripts
+
+	// register css
+}
+
+//add_action( 'init', 'ssa_init' );
